a1212992a0f01da9e828e0ab8ae7f4f13e38a6b0
hvmatay <127266047+hvmatay@users.noreply.github.com>
2024-04-22 18:15:44 +0000

    done

--- static/script.js
+++ static/script.js
@@ -1,1 +1,17 @@
 
+
+function toggleButton(){
+    if(document.getElementById("txtSearch").value == "")
+    {
+        document.getElementById("btnSearch").disabled = true;
+    }
+    else{
+        document.getElementById("btnSearch").disabled = false;
+    }
+}
+
+function getDate(){
+    let d = new Date();
+
+    document.getElementById("date").innerHTML = d;
+}
